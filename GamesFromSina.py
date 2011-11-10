6bb4721a24c4bc07a46e040e3bc45278b2812684
liuwanwei <liuwanwei@gmail.com>
2011-11-10 17:10:43 +0800

    First time real env running passed

--- GamesFromSina.py
+++ GamesFromSina.py
@@ -1,24 +1,46 @@
 #coding=utf-8
 import re
-from json import loads
-from urllib import urlencode
 import GetContent
+from WebApi import WebApi
+from time import sleep
 
 # 从新浪体育某个页面获取比赛信息
 class GamesFromSina(object):
-    def __init__(self):
+    def __init__(self):        
+        self._webApi=WebApi()
+        self.clear()
+        
+    def clear(self):
         self._leagueId=0    # 联赛名称
-        self._round=0       # 联赛轮次
+        self._round=0       # 联赛轮次        
         self._tournament=""
         self._hosts = []
         self._guests= []
         self._times = []
+        
         
     def printGames(self):
         print "%s 第%s轮" % (self._tournament, self._round)
         for i in range(0, len(self._times)):
             print "%s VS %s" % (self._hosts[i], self._guests[i])
             print "                         Date: "+self._times[i]
+        
+    def trimRoundString(self, string):
+        """删除赛事名称后面的’第N轮’信息
+        文件采用utf-8编码，要从搜索结果（gbk）中删除最后的“第几轮”
+        信息，要先把搜索模式串的编码改成gbk。
+        """
+        di='第'.decode('utf8').encode('cp936')
+        lun='轮'.decode('utf8').encode('cp936')
+        key='round'
+        pattern='.+?(?P<%s>%s.+?%s)' % (key, di, lun)
+        ret=re.search(pattern, string)
+        if None == ret:
+            print "%s not found" % pattern
+            return False
+        else:
+            count=len(ret.group(key))
+            return string[:-count]        
         
     def getTournament(self, html):
         """获取赛事名称
@@ -30,8 +52,14 @@ class GamesFromSina(object):
             print "%s not found" % pattern
             return None
         else:
-            self._tournament=ret.group(key)
-            self._tournament=self._tournament[:-5]
+            ret=self.trimRoundString(ret.group(key))
+            if(False == ret):
+                return False
+            else:
+                self._tournament = ret            
+                print ret
+                return True
+                
     
     def getTeams(self, html):
         """获取该轮次所有比赛主队和客队
@@ -70,70 +98,49 @@ class GamesFromSina(object):
         baseUrl="http://data.sports.sina.com.cn/yingchao/calendar/"
         params="?action=round&league_id=%s&round=%s" % (self._leagueId, self._round)
         url=(baseUrl+params)
+        print url
         return url    
                 
     def getGames(self, url):
         """获取赛事某轮次的所有比赛信息
         """
-        #TODO html=GetContent.getHtml(url)
-        html=open("sina.htm").read()
-        self.getTournament(html)
+        html=GetContent.getHtml(url)
+        #html=open("sina.htm").read()
+        if(False == self.getTournament(html)):
+            return False
         self.getTeams(html)
         self.getGameDates(html)
         
     def getRoundGames(self, leagueId, round):
         """公共接口，获取赛事某轮次的所有比赛信息
         """
+        self.clear()
         self._leagueId = leagueId
         self._round = round
         url=self.getRoundUrl()
-        self.getGames(url)    
-
-class WebApi(object):
-    def __init__(self):
-        self._domain='http://localhost/index.php?'
+        return self.getGames(url) 
         
-    def GBK2UTF8(self, string):
-        return string.decode('cp936').encode('utf8')
-    
-    def checkResult(self, ret):
-        """在对下一级的json.loads结果处理前，仍要调用json.loads
-        """
-        jsonData=loads(ret)
-        jsonData=jsonData['data']
-        jsonData=loads(jsonData)
-        id=jsonData['id']
-        if(id != False):
-            return True
-        else:
-            return False
-    
-    def addGame(self, tournament, hostTeam, guestTeam, dateTime, round):        
-        tournament=self.GBK2UTF8(tournament)
-        hostTeam=self.GBK2UTF8(hostTeam)
-        guestTeam=self.GBK2UTF8(guestTeam)
-        param=[('tournament',tournament), 
-               ('hostTeam',hostTeam), 
-               ('guestTeam',guestTeam), 
-               ('dateTime',dateTime),
-               ('round',round)]
-        encoded=urlencode(param)                
-        url=self._domain+'m=game&f=addGame&'+encoded+'&t=json'            
-        #print url
-        ret=GetContent.getHtml(url)        
-        print ret
-        return self.checkResult(ret)
+    def uploadRoundGames(self):        
+        count=len(obj._hosts)
+        for i in range(0, count):
+            if(False == self._webApi.addGame(obj._tournament, 
+                obj._hosts[i], obj._guests[i], obj._times[i], self._round)):
+                break
+        
+        
+    def getGamesInLeague(self, leagueId, maxRound):
+        for i in range(1, maxRound + 1):        
+            if( False == self.getRoundGames(leagueId, i)):
+                break
+            
+            self.uploadRoundGames()        
+            sleep(2)
+        
 
 if __name__ == '__main__':
-    leagueId=318
-    round=1
     obj=GamesFromSina()
-    obj.getRoundGames(leagueId, round)
-    #obj.printGames()
-    webApi=WebApi()
-    count=len(obj._hosts)
-    for i in range(0, count):
-        if(False == webApi.addGame(obj._tournament, 
-        obj._hosts[i], obj._guests[i], obj._times[i], round)):
-            break
+    leagueId=318
+    maxRound=30
+    obj.getGamesInLeague(leagueId, maxRound)
+    #obj.printGames()    
         
