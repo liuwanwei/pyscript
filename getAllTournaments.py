c35c7a2e78ebcbf1ccb409d4a13735374103fcd9
Liu WanWei <liuwanwei@gmail.com>
2012-11-01 17:35:28 +0800

    change to working mode

--- getAllTournaments.py
+++ getAllTournaments.py
@@ -1,10 +1,14 @@
 from GamesFromSina import GamesFromSina
 from GetContent import getHtml
 
+#http://data.sports.sina.com.cn/yingchao/calendar/?action=round&league_id=418&round=1
+#通过依次改变上方url中league_id的值，生成不同url，获取url代表哪项赛事信息。
+
 if __name__ == "__main__":
-    sina = GamesFromSina()
-    leagueMax = 397
-    for i in range(leagueMax):
-        url = sina.getRoundUrl(i, 1)
-        html = getHtml(url)
-        sina.getTournament(html)
+	sina = GamesFromSina()
+	leagueId = 400
+	while leagueId <= 450:
+		url = sina.getRoundUrl(leagueId, 1)
+		html = getHtml(url)
+		sina.getTournament(html)
+		leagueId = leagueId + 1
