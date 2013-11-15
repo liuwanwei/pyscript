5c06c252faae70fdd598347f5c779b10b128af05
liuwanwei <liuwanwei@gmail.com>
2013-11-15 15:07:02 +0800

    读写文件都已ok

--- family/qiaohu.py
+++ family/qiaohu.py
@@ -13,7 +13,7 @@ def add_category(name):
 
 def parse_a_tag(a):
 	matched = False
-	print(a['title'])
+	# print(a['title'])
 	for category in g_categories:
 		m=re.search(category['name'], a['title'])
 		if m != None:
@@ -30,10 +30,41 @@ def show_result():
 		for matched in category['result']:
 			print(matched['title'])
 
+def create_head_tag(soup, category_info):
+	head_tag = soup.new_tag('head')
+	title_tag = soup.new_tag('title')
+	title_tag.string = category_info['name']
+	head_tag.append(title_tag)
+	return head_tag
+
+def create_a_tag(soup, tag_info):
+	wrapper_tag = soup.new_tag('p')
+
+	a_tag = soup.new_tag('a')
+	a_tag['title'] = tag_info['title']
+	a_tag['href'] = tag_info['url']
+	a_tag.string = tag_info['title']
+
+	wrapper_tag.append(a_tag)
+	return wrapper_tag
+
 def generate_category():
 	for category in g_categories:
-		for result in category['result']:
-			h.a()
+		print(u'生成URL：' + category['name'] + ' %d' % len(category['result']))
+		soup = BeautifulSoup()
+		html_tag = soup.new_tag('html')
+
+		# html - head
+		html_tag.append(create_head_tag(soup, category))
+
+		# html - body
+		body_tag = soup.new_tag('body')
+		for result in category['result']:			
+			body_tag.append(create_a_tag(soup, result))		
+		html_tag.append(body_tag)
+
+		soup.append(html_tag)
+		open('./' + category['name'] + '.html', 'w').write(soup.prettify().encode('UTF-8'))
 
 # 准备视频分类数组
 for  name in g_patter_list:
@@ -54,3 +85,4 @@ for tag in divs:
 
 # 输出结果看看：）
 # show_result()
+generate_category()
