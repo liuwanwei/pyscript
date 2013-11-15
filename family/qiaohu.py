c32413ea5ba916adc2076d3dfbf3d064b1d1365f
liuwanwei <liuwanwei@gmail.com>
2013-11-15 11:50:16 +0800

    用4.3.2版BeautifulSoup进行解析。

--- family/qiaohu.py
+++ family/qiaohu.py
@@ -3,17 +3,18 @@
 #指示python用utf-8方式来处理代码，2.X版本默认非utf-8。
 
 import re
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
-categories = []
-pattern_list = [u'宝宝版', u'幼幼版', u'快乐版', u'成长版', u'学习版']
+g_categories = []
+g_patter_list = [u'宝宝版', u'幼幼版', u'快乐版', u'成长版', u'学习版']
 
 def add_category(name):
-	categories.append({'name' : name, 'result' : []})
+	g_categories.append({'name' : name, 'result' : []})
 
 def parse_a_tag(a):
 	matched = False
-	for category in categories:
+	print(a['title'])
+	for category in g_categories:
 		m=re.search(category['name'], a['title'])
 		if m != None:
 			category['result'].append({'url' : a['href'], 'title' : a['title']})
@@ -24,21 +25,32 @@ def parse_a_tag(a):
 		print(a['title'] + '- not matched')
 
 def show_result():
-	for category in categories:
+	for category in g_categories:
 		print category['name']
 		for matched in category['result']:
 			print(matched['title'])
 
+def generate_category():
+	for category in g_categories:
+		for result in category['result']:
+			h.a()
+
 # 准备视频分类数组
-for  name in pattern_list:
+for  name in g_patter_list:
 	add_category(name)
 
 html_doc = open('tongtongde.html').read()
-soup = BeautifulSoup(html_doc)
-divs = soup.findAll('div', {'id':'movie_title',})
+soup = BeautifulSoup(html_doc, 'lxml')
+
+# aes = soup.find_all('a', {'title':re.compile(u'成长版')})
+# print(len(aes))
+# for  a in aes:
+	# parse_a_tag(a)
+
+divs = soup.find_all('div', {'id':'movie_title'})
 for tag in divs:
         for a in tag('a'):
         	parse_a_tag(a)
 
 # 输出结果看看：）
-show_result()
+# show_result()
