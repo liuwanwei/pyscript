f70db5bf8f372e56d1217c5c184890cbfeac179e
liuwanwei <liuwanwei@gmail.com>
2015-04-16 11:40:54 +0800

    生成各种分辨率App Icon的代码

---
+++ helper/appicon.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+
+app_icon_dimentions = [29, 29*2, 29*3, \
+                       40, 40*2, 40*3, \
+                       #60*2 \ the same as 40*3
+                       60*3, \
+                       76, 76*2]
+
+def re_sample_app_icon(filename, subdir, width, height = None):
+    if height == None:
+        height = width
+
+    (head, sep, suffix) = filename.rpartition('.')
+    output = '%s/Icon-%d-%d.%s' % (subdir, width, height, suffix) 
+    subprocess.call(['sips', '-z', str(width), str(height), filename, '--out', output])
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage:\n\t./%s  the_path_of_file' % __file__)
+    else:
+        print(sys.argv)
+    
+    file_path = sys.argv[1]
+    
+    if os.path.exists(file_path):
+        filename = os.path.basename(file_path)
+        (subdir_name, sep, tail) = filename.rpartition('.')
+        print(subdir_name)
+        subdir = os.path.join(os.getcwd(), subdir_name)
+        print('subdir: %s' %  subdir)
+        if os.path.exists(subdir):
+            import shutil
+            shutil.rmtree(subdir)
+            os.mkdir(subdir)
+
+        for width in app_icon_dimentions:
+            re_sample_app_icon(file_path, subdir, width)
+
